a57cc5a5b3cff852510bea4dbc573f75f9af2cd3
Leonardo Lana <leolanavo@gmail.com>
2020-12-07 17:18:28 -0300

    Add nats integration

--- services/messages/src/messages.js
+++ services/messages/src/messages.js
@@ -3,27 +3,55 @@ import "regenerator-runtime/runtime.js";
 import Koa from "koa";
 import KoaRouter from "koa-router";
 import fetch from "node-fetch";
+import { connect } from "node-nats-streaming";
 
-const userURI = `http://${process.env.USERS_API_SERVICE_HOST}:${process.env.USERS_API_SERVICE_PORT}`;
+const {
+  BROKER_SERVICE_HOST,
+  BROKER_SERVICE_PORT_CLIENT,
+  USERS_API_SERVICE_HOST,
+  USERS_API_SERVICE_PORT,
+} = process.env;
+
+console.log("Initializing Server...");
+
+const NATS_URI = `nats://${BROKER_SERVICE_HOST}:${BROKER_SERVICE_PORT_CLIENT}`;
+const USER_URI = `http://${USERS_API_SERVICE_HOST}:${USERS_API_SERVICE_PORT}`;
+
+const stan = connect("broker-cluster", "messages-api", {
+  url: NATS_URI,
+});
+
 const port = 3001;
-
 const app = new Koa();
-
 const router = KoaRouter();
 
-router.get("/health-check", (ctx) => {
-  ctx.res.statusCode = 200;
-});
+stan.on("connect", () => {
+  console.log("Connected to NATS");
+  const replayAll = stan.subscriptionOptions().setDeliverAllAvailable();
 
-router.get("/", async (ctx) => {
-  const res = await fetch(userURI + "/messages");
-  console.log(res);
-  ctx.body = await res.text();
-});
+  const sub = stan.subscribe("new-message", replayAll);
+  sub.on("message", (msg) => {
+    console.log(`got message: ${msg.getData()}`);
+  });
 
-app.use(router.routes());
+  router.get("/", async (ctx) => {
+    const res = await fetch(USER_URI + "/messages");
+    console.log(res);
+    ctx.body = await res.text();
+  });
 
-app.listen({ port }, () => {
-  console.log(userURI);
-  console.log(`🚀 Server ready at http://localhost:${port}`);
+  router.get("/health-check", (ctx) => {
+    ctx.res.statusCode = 200;
+  });
+
+  router.get("/new-message", async (ctx) => {
+    stan.publish("new-message", `New Message ${Math.random()}`);
+    ctx.body = "New message sent";
+  });
+
+  app.use(router.routes());
+
+  app.listen({ port }, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  });
 });
